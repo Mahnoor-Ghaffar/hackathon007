63f72ee6ea53e747a56ef6c5cdff817f1e49ef97
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-02-07 19:00:04 +0500

    add
    add

--- src/app/api/userData/route.ts
+++ src/app/api/userData/route.ts
@@ -1,18 +1,12 @@
 import { NextResponse } from "next/server";
 import { auth, currentUser } from "@clerk/nextjs/server";
-import { getMyOrders } from "@/sanity/helpers";
 
 export async function GET() {
     try {
         const user = await currentUser();
         const { userId } = await auth();
-        let orders = [];
 
-        if (userId) {
-            orders = await getMyOrders(userId);
-        }
-
-        return NextResponse.json({ user, orders });
+        return NextResponse.json({ user });
     } catch (error) {
         return NextResponse.json({ error: "Failed to fetch user data" }, { status: 500 });
     }
